f719ee657056ed120c6a7acec751db3fb334da0a
ksuser <ksuser@admindeMacBook-Air.local>
2022-07-22 14:07:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,131 @@
+<template>
+  <div id="app">
+    <div class="header">已打开<span >{{chooseName}}</span>列表</div>
+    <myLevelList :menu="menuList" v-on:showsub="showToggle" ></myLevelList>
+  </div>
+</template>
+
+<script>
+import myLevelList from './components/level-list.vue'
+
+export default {
+  data(){
+      return{
+
+        menuList:[
+          {
+            name:'策略名称',
+            isSubShow:false,
+            subItems:[
+              {name:'账号爆破防护'},
+              {name:'API防护'},
+              {name:'网络爬虫放行'},
+            ]
+          },
+          {
+            name:'匹配类型',
+            isSubShow:false,
+            subItems:[
+              {name:'基础'},
+              {name:'自定义'},
+            ]
+          },
+          {
+            name:'匹配字段',
+            isSubShow:false,
+            subItems:[
+              {name:'Cookie'},
+              {name:'Referer'},
+              {name:'HTTP-Method'},
+              {name:'User-Agent'},
+              {name:'X-Forwarded-For'},
+              {name:'URI'},
+            ]
+          },
+          {
+            name:'逻辑符',
+            isSubShow:false,
+            subItems:[
+              {name:'包含'},
+              {name:'不包含'},
+              {name:'等于'},
+              {name:'不等于'},
+              {name:'长度小于'},
+              {name:'长度等于'},
+            ]
+          },
+          {
+            name:'匹配动作',
+            isSubShow:false,
+            subItems:[
+              {name:'阻断'},
+              {name:'放行'},
+              {name:'记录'},
+              {name:'JS挑战'},
+              {name:'验证码'}, 
+            ]
+          },
+          {
+            name:'优先级',
+            isSubShow:false,
+            subItems:[
+              {name:'1'},
+              {name:'2'},
+              {name:'3'},
+              {name:'4'},
+              {name:'5'},  
+            ]
+          },
+          {
+            name:'生效端口',
+            isSubShow:false,
+            subItems:[
+              {name:'1'},
+              {name:'443'},
+              {name:'8080'},
+            ]
+          },
+        ],
+      }
+    },
+  name: 'App',
+  components: {
+    myLevelList
+  },
+  methods:{
+  // 点击展开折叠菜单事件
+    showToggle:function(index){
+    this.menuList[index].isSubShow = !this.menuList[index].isSubShow;
+    },
+  },
+  computed:{
+    chooseName:function(){
+      let newlist=this.menuList.filter(r=>{
+        if(r.isSubShow) return true
+      })
+      let namelist=newlist.filter(()=>{
+        return namelist.name !== ''
+      })
+      console.log(namelist.name)
+      // return newlist 
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.header{
+  text-align: left;
+  font-size: 10px;
+}
+
+</style>
---
+++ src/components/level-list.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="asideBox">
+      <aside>
+        <ul class="asideMenu">
+          <li v-for="(item,index) in menu" v-bind:key="item.id">
+            <div class="oneMenu" @click="myClick(index)" :class="{active: item.isSubShow}">
+              <p>{{index+1}}-{{item.name}}
+              <span class="switch">+</span>
+              </p>              
+            </div>
+            <ul v-show="item.isSubShow">
+              <li v-for="subItem in item.subItems" v-bind:key="subItem.subid">
+                <div class="oneMenuChild">{{subItem.name}}</div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </aside>
+    </div>
+</template>
+
+<script>
+ export default {
+    props:['menu'],
+    data: function () {
+  return {
+    }
+  },
+  methods: {
+      myClick (index) {
+        this.$emit('showsub',index)
+      }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+  $menuBackColor:#f1f1f1;
+  $menuListH2:#8fbfef;
+  .asideBox{
+    height: 100%;
+    width: 100%;
+    aside{
+      background: $menuBackColor;
+      height: 100%;
+      .asideMenu{
+        .oneMenu{
+          height: 50px;
+          line-height: 50px;
+          font-size: 18px;
+          font-weight: normal;
+          color: #454343;
+          border-bottom: 1px solid #669cd9;
+          .switch{
+                cursor: pointer;
+                position: absolute;
+                right: 30px;                
+                width: 10px;
+                height: 10px;
+          }
+        }
+        .oneMenuChild{
+          padding: 0 20px 0 0;
+          height: 40px;
+          line-height: 40px;
+          background: $menuBackColor;
+          border-bottom: 1px solid #ffffff;
+          color: #454343;
+          font-size: 18px;
+        }
+        .active {
+          background: rgba(93, 93, 233, 0.737);
+        }
+      }
+    }
+  }
+</style>
